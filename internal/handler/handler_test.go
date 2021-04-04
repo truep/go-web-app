813c50d024c739f880102609733ca231dae82412
Матюхин Данил <post7@intersvyaz.net>
2021-04-04 20:06:36 +0500

    Дописал тесты для handler.Hello, не уверен, что для ошибки корректно написан тест

--- internal/handler/handler_test.go
+++ internal/handler/handler_test.go
@@ -12,18 +12,35 @@ import (
 
 func TestHandler_Hello(t *testing.T) {
 	tests := []struct {
-		name     string
-		joke     *api.JokeResponse
-		err      error
-		codeWant int
-		bodyWant string
+		name       string
+		customJoke string
+		joke       *api.JokeResponse
+		err        error
+		codeWant   int
+		bodyWant   string
 	}{
 		{
-			name:     "simple test",
-			joke:     &api.JokeResponse{Joke: "test joke"},
-			err:      nil,
-			codeWant: 200,
-			bodyWant: "test joke",
+			name:       "Testing simple joke",
+			customJoke: "",
+			joke:       &api.JokeResponse{Joke: "test joke"},
+			err:        nil,
+			codeWant:   200,
+			bodyWant:   "test joke",
+		},
+		{
+			name:       "Testing custom joke",
+			customJoke: "Custom joke",
+			joke:       &api.JokeResponse{Joke: "some stupid joke"},
+			err:        nil,
+			codeWant:   200,
+			bodyWant:   "Custom joke",
+		},
+		{
+			name:     "Testing error case",
+			joke:     &api.JokeResponse{},
+			err:      &http.ProtocolError{},
+			codeWant: http.StatusInternalServerError,
+			bodyWant: http.StatusText(http.StatusInternalServerError),
 		},
 	}
 	for _, tt := range tests {
@@ -31,7 +48,7 @@ func TestHandler_Hello(t *testing.T) {
 			apiMock := mocks.Client{}
 			apiMock.On("GetJoke").Return(tt.joke, tt.err)
 
-			h := NewHandler(&apiMock, "")
+			h := NewHandler(&apiMock, tt.customJoke)
 
 			req, _ := http.NewRequest("GET", "/", nil)
 			rr := httptest.NewRecorder()
@@ -41,12 +58,14 @@ func TestHandler_Hello(t *testing.T) {
 			gotRaw, _ := ioutil.ReadAll(rr.Body)
 			got := string(gotRaw)
 
-			if got != tt.bodyWant {
-				t.Errorf("wrong response body %s want %s", got, tt.bodyWant)
+			if tt.err == nil {
+				if got != tt.bodyWant {
+					t.Errorf("wrong response body '%s' want '%s'", got, tt.bodyWant)
+				}
 			}
 
 			if status := rr.Result().StatusCode; status != tt.codeWant {
-				t.Errorf("wrong response status %d want %d", status, tt.codeWant)
+				t.Errorf("wrong response status '%d' want '%d'", status, tt.codeWant)
 			}
 
 		})
